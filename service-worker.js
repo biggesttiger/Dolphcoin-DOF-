0ebdc2f934807ce6282e761c91111de98df13684
biggesttiger <biggesttiger9@gmail.com>
2025-02-10 21:39:17 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,24 @@
-self.addEventListener('install', function(event) {
+const CACHE_NAME = 'dolph-coin-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js',
+  'https://imgur.com/U1v2iKY', // 192x192 icon
+  'https://imgur.com/your-512x512-icon.png' // Replace with your 512x512 icon link
+];
+
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('static-v1').then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        '/icon-192x192.png',
-        '/icon-512x512.png'
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
     })
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
   );
