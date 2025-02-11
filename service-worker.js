a166bf6973c484456581a5251cd067c4fb468c34
biggesttiger <biggesttiger9@gmail.com>
2025-02-11 01:00:59 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,22 +3,26 @@ const ASSETS = [
   '/',
   '/index.html',
   '/styles.css',
-  '/app.js',
-  'https://imgur.com/U1v2iKY', // 192x192 icon
-  'https://imgur.com/h7FS5Bp'  // 512x512 icon
+  '/script.js',
+  'https://i.imgur.com/0NLj45g.png',
+  'https://unpkg.com/swiper/swiper-bundle.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css',
+  'https://unpkg.com/swiper/swiper-bundle.min.js'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});(response) => {
       return response || fetch(event.request);
     })
   );
